5cb75ef2670020d3ad8d67161ed919676127da07
bami <1992bami@gmail.com>
2022-06-10 10:23:45 +0900

    Add core app and test

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,7 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-    
+
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
@@ -18,5 +18,5 @@ class CalcTests(SimpleTestCase):
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
-        
+
         self.assertEqual(res, 5)
